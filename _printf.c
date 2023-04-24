f16294f2ec8b3180ee805d1cd50dc2f1256b94af
Abdelouafi Er-raqui <abdelwafir@gmail.com>
2023-04-24 20:03:23 +0000

    first task

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdarg.h>
 /**
  * _printf - print string to stdout
  * @format: string to display
@@ -6,5 +7,42 @@
 
 int _printf(const char *format, ...)
 {
+	va_list args;
+	va_start(args, format);
 
+	int count = 0;
+	const char *p = format;
+
+	while (*p != '\0') {
+		if (*p == '%') {
+			switch (*(++p)) {
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's': {
+						  char *str = va_arg(args, char *);
+						  int len = _strlen(str);
+						  write(1, str, len);
+						  count += len;
+						  break;
+					  }
+				case '%':
+					  _putchar('%');
+					  count++;
+					  break;
+				default:
+					  _putchar('%');
+					  _putchar(*p);
+					  count += 2;
+			}
+		} else {
+			_putchar(*p);
+			count++;
+		}
+		p++;
+	}
+
+	va_end(args);
+	return count;
 }
