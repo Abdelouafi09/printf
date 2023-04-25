7d2505a15a9077472317405d36a044a12f59cdb7
Abdelouafi09 <97417156+Abdelouafi09@users.noreply.github.com>
2023-04-25 19:35:20 +0000

    Update main.h

--- main.h
+++ main.h
@@ -5,10 +5,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int _putchar(char c);
 int _print_number(int n);
 int _strlen(char *s);
 int _puts(char *s);
-
 int _printf(const char *format, ...);
-
 #endif
